6a18add9cd53839a79607ec9c554ae04ac12ac62
vardhan1228 <vvardhan1211@gmail.com>
2026-08-28 12:18:19 +0530

    Use ConfigMap instead of Secret, auto-created by Terraform after MySQL provisioning

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,6 +18,14 @@ output "mysql_fqdn" {
   value = azurerm_mysql_flexible_server.mysql.fqdn
 }
 
+output "mysql_admin_username" {
+  value = azurerm_mysql_flexible_server.mysql.administrator_login
+}
+
+output "mysql_admin_password" {
+  value = var.mysql_admin_password
+}
+
 output "get_credentials_command" {
   value = "az aks get-credentials --resource-group ${azurerm_resource_group.data.name} --name ${azurerm_kubernetes_cluster.aks.name}"
 }
