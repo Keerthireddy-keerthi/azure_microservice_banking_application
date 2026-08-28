d34a6d2adb777228f6f2919c2b1b370a4751fcd8
vardhan1228 <vvardhan1211@gmail.com>
2026-08-28 15:01:48 +0530

    Remove random ACR suffix, hardcode bankingappacr in env

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,11 +1,5 @@
-resource "random_string" "acr_suffix" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.project_name}acr${random_string.acr_suffix.result}"
+  name                = "${var.project_name}acr"
   resource_group_name = azurerm_resource_group.data.name
   location            = azurerm_resource_group.data.location
   sku                 = var.acr_sku
