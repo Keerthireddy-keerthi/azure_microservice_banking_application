df292b286bf1649cff89790ebdbd2708f9a2fb40
Keerthireddy <puthalapattukeerthireddy9@gmail.com>
2026-08-31 10:12:01 +0530

    Rename Azure resources in network.tf
    Renamed resources for clarity and consistency.

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,30 +1,30 @@
-resource "azurerm_resource_group" "data" {
+resource "azurerm_resource_group" "dataa" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "vnett" {
   name                = "${var.project_name}-vnet"
   location            = azurerm_resource_group.data.location
   resource_group_name  = azurerm_resource_group.data.name
   address_space       = var.vnet_address_space
 }
 
-resource "azurerm_subnet" "aks_subnet" {
+resource "azurerm_subnet" "aks_subnett" {
   name                 = "${var.project_name}-aks-subnet"
   resource_group_name  = azurerm_resource_group.data.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.aks_subnet_prefix
 }
 
-resource "azurerm_subnet" "db_subnet" {
+resource "azurerm_subnet" "db_subnett" {
   name                 = "${var.project_name}-db-subnet"
   resource_group_name  = azurerm_resource_group.data.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.db_subnet_prefix
 
   delegation {
-    name = "mysql-delegation"
+    name = "mysql-delegationn"
     service_delegation {
       name    = "Microsoft.DBforMySQL/flexibleServers"
       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
@@ -32,12 +32,12 @@ resource "azurerm_subnet" "db_subnet" {
   }
 }
 
-resource "azurerm_private_dns_zone" "mysql_dns" {
+resource "azurerm_private_dns_zone" "mysql_dnss" {
   name                = "${var.project_name}.mysql.database.azure.com"
   resource_group_name = azurerm_resource_group.data.name
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link" {
+resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_linkk" {
   name                  = "${var.project_name}-mysql-dns-link"
   resource_group_name   = azurerm_resource_group.data.name
   private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
