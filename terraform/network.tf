ba65a4003d923bcaa734f960464c1a8807f3146e
Keerthireddy <puthalapattukeerthireddy9@gmail.com>
2026-08-31 10:13:26 +0530

    Rename virtual network and subnets for consistency

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,21 +4,21 @@ resource "azurerm_resource_group" "dataa" {
 }
 
 resource "azurerm_virtual_network" "vnett" {
-  name                = "${var.project_name}-vnet"
+  name                = "${var.project_name}-vnett"
   location            = azurerm_resource_group.data.location
   resource_group_name  = azurerm_resource_group.data.name
   address_space       = var.vnet_address_space
 }
 
 resource "azurerm_subnet" "aks_subnett" {
-  name                 = "${var.project_name}-aks-subnet"
+  name                 = "${var.project_name}-aks-subnett"
   resource_group_name  = azurerm_resource_group.data.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.aks_subnet_prefix
 }
 
 resource "azurerm_subnet" "db_subnett" {
-  name                 = "${var.project_name}-db-subnet"
+  name                 = "${var.project_name}-db-subnett"
   resource_group_name  = azurerm_resource_group.data.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.db_subnet_prefix
@@ -38,8 +38,8 @@ resource "azurerm_private_dns_zone" "mysql_dnss" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_linkk" {
-  name                  = "${var.project_name}-mysql-dns-link"
-  resource_group_name   = azurerm_resource_group.data.name
-  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
+  name                  = "${var.project_name}-mysql-dns-linkk"
+  resource_group_name   = azurerm_resource_group.dataa.name
+  private_dns_zone_name = azurerm_private_dns_zone.mysql_dnss.name
+  virtual_network_id    = azurerm_virtual_network.vnett.id
 }
